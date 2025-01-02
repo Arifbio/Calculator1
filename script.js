57f5b4a49ed196890dcfedb1cf0266741d0e93c4
Arifbio <arif.hassan42201@gmail.com>
2025-01-03 01:18:59 +0500

    adding a Project 1 (Calculator) to github

---
+++ script.js
@@ -0,0 +1,11 @@
+var button = document.getElementById('btn1');
+
+// Add a click event listener to the button
+button.addEventListener('click', function() {
+    alert('Button was clicked!');
+});
+
+function number(num = 0 ){
+  console.log('Button was clicked! '+num);
+  console.log('Button was clicked! '+num);
+}
